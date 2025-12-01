1501b6b2fef3cc1f09ae90c1b7e6f0afa32e3401
PwNT5Un3 <max@welsch.io>
2025-12-01 17:24:51 +0100

    added a market-maker class to provide initial offers

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ class OrderBook:
 			if any(order['qty'] > 0 for order in orders)
 		}
 
-	
 	def get_book_keys(self):
 		return self.book.keys()
 	
@@ -41,16 +40,16 @@ def	generate_order_id(trader_id: str):
 	return (trader_id + "|" + str(random.getrandbits(64)))
 
 full_book = {
-    "NDYS": {"bids": OrderBook(), "offers": OrderBook(), "name": "NovaDyne Systems"},
-    "HLXN": {"bids": OrderBook(), "offers": OrderBook(), "name": "Helixion Interactive"},
-    "BCIC": {"bids": OrderBook(), "offers": OrderBook(), "name": "BlueCrest Infrastructure Corp."},
-    "PBNJ": {"bids": OrderBook(), "offers": OrderBook(), "name": "PBNJ Foods & Consumer Brands"},
-    "QHTC": {"bids": OrderBook(), "offers": OrderBook(), "name": "Quantum Harbor Technologies"},
-    "SWVC": {"bids": OrderBook(), "offers": OrderBook(), "name": "StellarWave Communications"},
-    "GMEG": {"bids": OrderBook(), "offers": OrderBook(), "name": "GreenMantle Energy Group"},
-    "ASTR": {"bids": OrderBook(), "offers": OrderBook(), "name": "Asterion Automotive"},
-    "RVFH": {"bids": OrderBook(), "offers": OrderBook(), "name": "Riverstone Financial Holdings"},
-    "ALEM": {"bids": OrderBook(), "offers": OrderBook(), "name": "ArcLight Entertainment Media"}
+    "NDYS": {"bids": OrderBook(), "offers": OrderBook(), "name": "NovaDyne Systems", "share_num": 1000000, "initial_share_price": 17054},
+    "HLXN": {"bids": OrderBook(), "offers": OrderBook(), "name": "Helixion Interactive", "share_num": 100000, "initial_share_price": 1076},
+    "BCIC": {"bids": OrderBook(), "offers": OrderBook(), "name": "BlueCrest Infrastructure Corp.", "share_num": 50000, "initial_share_price": 4539},
+    "PBNJ": {"bids": OrderBook(), "offers": OrderBook(), "name": "PBNJ Foods & Consumer Brands", "share_num": 200000, "initial_share_price": 10022},
+    "QHTC": {"bids": OrderBook(), "offers": OrderBook(), "name": "Quantum Harbor Technologies", "share_num": 600000, "initial_share_price": 4567},
+    "SWVC": {"bids": OrderBook(), "offers": OrderBook(), "name": "StellarWave Communications", "share_num": 2000000, "initial_share_price": 34564},
+    "GMEG": {"bids": OrderBook(), "offers": OrderBook(), "name": "GreenMantle Energy Group", "share_num": 150000, "initial_share_price": 4235},
+    "ASTR": {"bids": OrderBook(), "offers": OrderBook(), "name": "Asterion Automotive", "share_num": 120000, "initial_share_price": 7463},
+    "RVFH": {"bids": OrderBook(), "offers": OrderBook(), "name": "Riverstone Financial Holdings", "share_num": 170000, "initial_share_price": 24617},
+    "ALEM": {"bids": OrderBook(), "offers": OrderBook(), "name": "ArcLight Entertainment Media", "share_num": 450000, "initial_share_price": 5746}
 }
 
 @dataclass
@@ -65,6 +64,7 @@ class Trader:
 		self.positions: List[position] = []
 		self.open_orders: Dict[str, Order] = {}
 		self.Broker = Broker
+		all_traders[id] = self
 
 	def submit_limit_order(self, ticker: str, qty: int, price: int, side: str):
 		order_id = generate_order_id(self.id)
@@ -81,6 +81,17 @@ class Trader:
 
 	def print_orders(self):
 		print(self.open_orders)
+
+class market_maker(Trader):
+	def give_initial_positions(self):
+		for ticker in full_book.keys():
+			new_position = position(ticker=ticker, qty=full_book[ticker]['share_num'])
+			self.positions.append(new_position)
+	
+	def make_initial_offers(self):
+		for share in self.positions:
+			self.submit_limit_order(ticker=share.ticker, qty=share.qty, price=full_book[share.ticker]['initial_share_price'], side='sell')
+
 
 all_traders = {}
 
@@ -125,36 +136,14 @@ def fill_order(side: str, order: Order, price: int):
 	
 
 if __name__ == "__main__":
-	test = Trader("BR1", 1000, "BR")
-	all_traders[test.id] = test
-	i = 0
-	while (i < 3):
-		test.submit_limit_order("PBNJ", 100, i, 'buy')
-		i += 1
-	test_entry = list(test.open_orders.items())[1]
-	print(test_entry)
-	if (test_entry[1]['side']) == 'buy':
-		for key in full_book[test_entry[1]['ticker']]['bids'].get_book_keys():
-			RestingOrders = full_book[test_entry[1]['ticker']]['bids'].get_price_level(key)
-			for RestingOrder in RestingOrders:
-				if RestingOrder['order_id'] == test_entry[1]['order_id']:
-					print(RestingOrder)
-	else:
-		for key in full_book[test_entry[1]['ticker']]['offers'].get_book_keys():
-			RestingOrders = full_book[test_entry[1]['ticker']]['bids'].get_price_level(key)
-			for RestingOrder in RestingOrders:
-				if RestingOrder['order_id'] == test_entry[1]['order_id']:
-					print(RestingOrder)
-	for key in full_book[test_entry[1]['ticker']]['bids'].get_book_keys():
-			RestingOrders = full_book[test_entry[1]['ticker']]['bids'].get_price_level(key)
-			for RestingOrder in RestingOrders:
-				if RestingOrder['order_id'] == test_entry[1]['order_id']:
-					RestingOrder['qty'] = 0
-					print(RestingOrder)
-	print(test_entry)
-	print("-----------------------------------------------")
-	for order in test.open_orders:
-		print(test.open_orders.get(order))
-	test.clear_orders()
-	for order in test.open_orders:
-		print(test.open_orders.get(order))
+	market_maker = market_maker("MM", 0, "MM")
+	market_maker.give_initial_positions()
+	market_maker.make_initial_offers()
+	for ticker in full_book.keys():
+		full_book[ticker]['bids'].print_book()
+	print("--------------------------------------------------------------")
+	for ticker in full_book.keys():
+		full_book[ticker]['offers'].print_book()
+	print(all_traders)
+	print(all_traders['MM'].print_orders())
+	
